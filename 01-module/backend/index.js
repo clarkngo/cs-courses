45fcaa0dcfda4c2090902957efea5080d59ac385
Clark Ngo <30854108+clarkngo@users.noreply.github.com>
2024-11-07 18:16:25 +0000

    hello world

--- 01-module/backend/index.js
+++ 01-module/backend/index.js
@@ -8,6 +8,10 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.post('/api/chat', async (req, res) => {
   const userMessage = req.body.content || "Please provide a message.";
 
