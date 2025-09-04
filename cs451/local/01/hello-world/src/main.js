070ee5449554cee3cce7b8621b21fdd47982c099
Clark Ngo <clarkngo@gmail.com>
2025-09-04 10:24:15 -0700

    update

---
+++ cs451/local/01/hello-world/src/main.js
@@ -0,0 +1,24 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Hello World!</h1>
+    <h2>Welcome to My Page</h2>
+    <p>This is a simple webpage created with JavaScript, HTML, and CSS.</p>
+    
+    <h2>About</h2>
+    <p>This section provides some information about the project. 
+    You can add more details here to describe what this page is about.</p>
+    
+    <h2>Features</h2>
+    <p>Here are some features of this simple setup:</p>
+    <ul>
+      <li>Heading and paragraph structure</li>
+      <li>Clean and minimal setup</li>
+      <li>Easy to extend</li>
+    </ul>
+    
+    <h2>Contact</h2>
+    <p>If you want to reach out, you can add contact details here.</p>
+  </div>
+`
