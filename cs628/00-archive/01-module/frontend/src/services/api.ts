8bec8fab23fb109faab894ba3e4cfeae3722be78
Clark Ngo <clarkngo@gmail.com>
2025-11-08 12:53:37 -0800

    update

--- cs628/00-archive/01-module/frontend/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Use the environment variable
-const API_URL = process.env.REACT_APP_API_URL;
-
-export const fetchHelloWorld = async () => {
-  const response = await axios.get(`${API_URL}/hello`);
-  return response.data;
-};
